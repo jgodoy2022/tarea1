6ff5cdb97c8c402e5538532f8f5c597c21a72bc4
jgodoy2022@ <jgodoy2022@udec.cl>
2023-09-27 14:31:14 -0300

    mas metodos

--- src/OrdenCompra.java
+++ src/OrdenCompra.java
@@ -1,7 +1,18 @@
+import java.util.Date;
+
 public class OrdenCompra{
-    private int fecha;
+    private Date fecha;
     private String estado;
-    public int PrecioSinIVA(){
-        return;
+    public float PrecioSinIVA(){
+        return 0;
+    }
+    public float calcIVA(){
+        return 0;
+    }
+    public float calcPrecio(){
+        return 0;
+    }
+    public float calcPeso(){
+        return 0;
     }
 }
