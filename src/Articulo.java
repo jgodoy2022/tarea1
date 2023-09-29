2d319e152add6b02e6dcf724c03c3304ee072fe6
gzambrano2022 <gzambrano2022@udec.cl>
2023-09-29 10:48:20 -0300

    añadir getter y algunos setter

--- src/Articulo.java
+++ src/Articulo.java
@@ -3,4 +3,20 @@ public class Articulo {
     private String nombre;
     private String descripcion;
     private float precio;
+
+    public float getPeso(){
+        return this.peso;
+    }
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    public float getPrecio(){
+        return this.precio;
+    }
 }
--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,17 @@
 public class Cliente {
     private String nombre;
     private String rut;
+
+    public String getNombre(){
+        return this.nombre;
+    }
+
+    public String getRut() {
+        return rut;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
